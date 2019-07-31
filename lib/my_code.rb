6065d29b3cb7b2f43d389017b20c08014d6cf88a
Brian Ly <brian.kyang.ly@gmail.com>
2019-07-31 18:50:26 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -21,76 +21,42 @@ def map_to_no_change(array)
 end
 
 
-####  describe "map_to_double returns an array with the original values multiplied by 2" do
-####    it "transforms correctly" do
-  ####    expect(map_to_double([1, 2, 3, -9])).to eq([2, 4, 6, -18])
-
 def map_to_double(array)
   arr = []
   array.each { |e| arr << e*2}
   arr
 end
 
-
-####  describe "map_to_square returns an array with the original values squared" do
-  ####  it "transforms correctly" do
-  ####    expect(map_to_square([1, 2, 3, -9])).to eq([1, 4, 9, 81])
-
 def map_to_square(array)
-  array.collect do |e|
-    e**2
-  end
+  arr = []
+  array.each {|e| arr << e**2}
+  arr
 end
-####  describe "reduce_to_total returns a running total when not given a starting point" do
-  ###  it "reduces correctly" do
-    ###  source_array = [1,2,3]
-    ###  expect(reduce_to_total(source_array)).to eq(6)
-
-###  describe "reduce_to_total returns a running total when given a starting point" do
-  ###  it "reduces correctly" do
-  ###    source_array = [1,2,3]
-    ###  starting_point = 100
-    ###  expect(reduce_to_total(source_array, starting_point)).to eq(106)
 
 def reduce_to_total(array, start = 0)
-  array.reduce(start) do |t, e|
-    t + e
+  total = start
+  array.each do |e|
+    total += e
   end
+  total
 end
-###  describe "reduce_to_all_true returns true when all values are truthy" do
-###    it "reduces correctly" do
-###      source_array = [1, 2, true, "razmatazz"]
-  ###    expect(reduce_to_all_true(source_array)).to be_truthy
 
 def reduce_to_all_true(array)
-  array.reduce do |t, e|
-    t if e
-  end
+  counter = 1
+  value = array[0]
+  while counter < array.length
+    array[counter] && value ? value = true : value = false
+    counter += 1
+  end #while
+  value
 end
 
-##  describe "reduce_to_all_true returns false when any value is false" do
-##    it "reduces correctly" do
-  ##    source_array = [1, 2, true, "razmatazz", false]
-  ##    expect(reduce_to_all_true(source_array)).to be_falsy
-
-
-##  describe "reduce_to_any_true returns true when a truthy value is present" do
-##    it "reduces correctly" do
-  ##    source_array = [ false, nil, nil, nil, true]
-  ##    expect(reduce_to_any_true(source_array)).to eq(true)
-
-#  describe "reduce_to_any_true returns false when no truthy value is present" do
-#    it "reduces correctly" do
-  #    source_array = [ false, nil, nil, nil]
-  #    expect(reduce_to_any_true(source_array)).to eq(false)
-
 def reduce_to_any_true(array)
-  bool = false
-  array.each do |e|
-    bool = bool || e
-  end
-  if bool == nil
-    bool = false
-  end
-  bool
+  counter = 1
+  value = array[0]
+  while counter < array.length
+    array[counter] || value ? value = true : value = false
+    counter += 1
+  end #while
+  value
 end
